7de3f29023e1bd012ce5c92ad835613d031cbbec
Kelly Alsobrook <the-wash@Kellys-MacBook-Pro.local>
2022-03-05 18:36:11 -0700

    updated changes

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function showError(input, message) {
   small.innerText = message;
 }
 
-function emailIsValid (email) {
-  return /\S+@\S+\.\S+/.test(email)
+function emailIsValid (input) {
+  return /\S+@\S+\.\S+/.test(input);
 }
 
 function showSuccess(input) {
@@ -20,32 +20,45 @@ function showSuccess(input) {
   formControl.className = 'form-control success'
 }
 
+// check input lengths
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less that ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if(input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if(username.value === '') {
-    showError(username, 'This field is required');
-  } else {
-    showSuccess(username);
-  }
-
-  if(email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!emailIsValid(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
-
-  if(password.value === '') {
-    showError(password, 'This field is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if(password2.value === '') {
-    showError(password2, 'This field is required');
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
+  // passing min, and max
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 26);
+  checkPasswordsMatch(password, password2)
 });
